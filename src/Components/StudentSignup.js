d3217639a2a52cd4115c020b6bd56655ad888388
Sam-JD-7 <samjebadurait.20cse@kongu.edu>
2023-04-26 13:50:21 +0530

    signup

--- src/Components/StudentSignup.js
+++ src/Components/StudentSignup.js
@@ -2,33 +2,6 @@ import React from 'react'
 import { useState} from 'react'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
-const add=(req,res)=>
-{
-    alert(req.body.name)
-//     axios.post("http://localhost:3002/student/createstudent",
-//     {
-//         "studentname": "query",
-//         "studentemail": "samjebadurait.20cse@kongu.edu",
-//         "password":"12345678",
-//         "studentregno": "20csr186",
-//         "studentdob": "2002-08-25",
-//         "fathersname": "Hero",
-//         "mothersname": "Batman",
-//         "gender": "male",
-//         "address": "Kangayam",
-//         "mentorname": "savitha",
-//         "mentormail": "savitha@kkongu.edu"
-//     })
-//     .then(res=>
-//         {
-//             console.log(res)
-//         })
-//         .catch(err=>
-//             {
-//                 console.log(err)
-//             })
-
-}
 // export const Student=()=>
 // {
     
@@ -161,27 +134,27 @@ export const Student=()=>{
         
             <div class="card-body p-4">
         
-                <form action="/add" method="post" id="add-form" enctype="multipart/form-data">
+                <form action={submit} id="add-form" enctype="multipart/form-data">
         
                     <div class="mb-3">
         
-                        <label>Name</label>
+                        <label for="name">Name</label>
                         
-                        <input type="text" name="name" class="form-control form-control-lg" placeholder="Enter name" required />  
+                        <input type="text" name="name" value={user} class="form-control form-control-lg" placeholder="Enter name" onChange={(e)=>setUser(e.target.value)} required />  
                     </div>
         
                     <div class="mb-3">
                     
                         <label for="email">Email</label>
                         
-                        <input type="email" name="email" class="form-control form-control-lg" placeholder="Enter email" required />
+                        <input type="email" name="email" value={email} class="form-control form-control-lg" placeholder="Enter email" onChange={(e)=>setEmail(e.target.value)} required />
                     
                     </div>
                     <div class="mb-3">
 
                         <label for="regno">Register Number</label>
                         
-                        <input type="text" name="regno" class="form-control form-control-lg" placeholder="Register Number" required />
+                        <input type="text" name="regno" value={regno} class="form-control form-control-lg" placeholder="Register Number" onChange={(e)=>setRegno(e.target.value)} required />
                         
                     </div>
 
@@ -189,7 +162,15 @@ export const Student=()=>{
 
                         <label for="password">Password</label>
                         
-                        <input type="password" name="password" class="form-control form-control-lg" placeholder="Password" required />
+                        <input type="password" name="password" value={password} class="form-control form-control-lg" placeholder="Password" onChange={(e)=>setPassword(e.target.value)} required />
+                        
+                    </div>
+
+                    <div class="mb-3">
+
+                        <label for="cpassword">Correct Password</label>
+                        
+                        <input type="password" name="cpassword" value={Cpassword} class="form-control form-control-lg" placeholder="Password" onChange={(e)=>setCpassword(e.target.value)} required />
                         
                     </div>
 
@@ -197,7 +178,8 @@ export const Student=()=>{
 
                         <label for="gender">Gender</label>
                         
-                        <select type="password" name="password" class="form-control form-control-lg" placeholder="Password" required >
+                        <select value={gender} name="gender" class="form-control form-control-lg"  onChange={(e)=>setGender(e.target.value)} required >
+                        <option value="" selected disabled hidden>--Choose here--</option>
                         <option value="Male">Male</option>
                         <option value="Female">Female</option>
                         </select>
@@ -206,7 +188,7 @@ export const Student=()=>{
 
                         <label for="dob">DOB</label>
                         
-                        <input type="date" name="dob" class="form-control form-control-lg" placeholder="DOB" required />
+                        <input type="date" name="dob" value={dob} class="form-control form-control-lg" onChange={(e)=>setDob(e.target.value)} required />
                         
                     </div>
 
@@ -214,7 +196,7 @@ export const Student=()=>{
 
                         <label for="phone">Phone</label>
                         
-                        <input type="tel" name="phone" class="form-control form-control-lg" placeholder="Enter phone" required />
+                        <input type="tel" name="phone" class="form-control form-control-lg" placeholder="Enter phone" onChange={(e)=>setUser(e.target.value)} required />
                         
                     </div>
              */}
@@ -230,7 +212,7 @@ export const Student=()=>{
 
                         <label for="mentorname">Mentor Name</label>
                         
-                        <input type="text" name="mentorname" class="form-control form-control-lg" placeholder="Mentor Name" required />
+                        <input type="text" name="mentorname" value={mentor} class="form-control form-control-lg" placeholder="Mentor Name" onChange={(e)=>setMentor(e.target.value)} required />
                         
                     </div>
 
@@ -238,7 +220,7 @@ export const Student=()=>{
 
                         <label for="mentormail">Mentor Email</label>
                         
-                        <input type="text" name="mentormail" class="form-control form-control-lg" placeholder="MentorEmail" required />
+                        <input type="email" name="mentormail" value={mentormail} class="form-control form-control-lg" placeholder="MentorEmail" onChange={(e)=>setMentormail(e.target.value)} required />
                         
                     </div>
 
@@ -246,14 +228,14 @@ export const Student=()=>{
 
                         <label for="mothername">Mother's Name</label>
                         
-                        <input type="text" name="mothername" class="form-control form-control-lg" placeholder="Mother's name" required />
+                        <input type="text" name="mothername" value={mothername} class="form-control form-control-lg" placeholder="Mother's name" onChange={(e)=>setMothername(e.target.value)} required />
                         
                     </div>
                     <div class="mb-3">
 
                         <label for="fathername">Father's Name</label>
                         
-                        <input type="text" name="fathername" class="form-control form-control-lg" placeholder="Father's name" required />
+                        <input type="text" name="fathername" value={fathername} class="form-control form-control-lg" placeholder="Father's name" onChange={(e)=>setFathername(e.target.value)} required />
                         
                     </div>
 
@@ -261,7 +243,7 @@ export const Student=()=>{
 
                         <label for="address">Address</label>
                         
-                        <textarea type="text" name="address" class="form-control form-control-lg" placeholder="Address" required />
+                        <textarea type="text" name="address" value={address} class="form-control form-control-lg" placeholder="Address" onChange={(e)=>setAddress(e.target.value)} required />
                         
                     </div>
             
